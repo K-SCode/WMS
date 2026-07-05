e9107dae15d0b37737c93d56d7d16fff9bd138ff
Konrad <231018054+K-SCode@users.noreply.github.com>
2026-07-05 18:21:20 +0200

    feat: add DbContext

---
+++ WMS.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WMS.Core.Entities;
+
+namespace WMS.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Product {  get; set; }
+        public DbSet<Localization> Localization { get; set; }
+        public DbSet<Stock> Stock { get; set;  }
+        //TO DO:
+        //CREATE FLUENT API
+        //CHECK HOW I SHOULD CREATE A DBCONTEX
+        //ADD MIGRATIONS
+        //ADD REPOSITORY
+        //ADD SERVICES
+        //ADD FLUENTVALIDATION
+        //ADD MAPPER
+        //ADD 
+    }
+}
